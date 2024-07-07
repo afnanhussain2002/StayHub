7555593c3e5e7dd57aa199e8d232b13f2b109e2c
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-07 10:17:34 +0600

    Responsive logo

--- client/src/sheard/header/Logo.jsx
+++ client/src/sheard/header/Logo.jsx
@@ -3,8 +3,8 @@ import { FaBed } from "react-icons/fa6";
 
 const Logo = () => {
     return (
-        <div className='w-40 flex items-center text-3xl text-white font-bold gap-1'>
-            <span className="text-4xl">
+        <div className='w-40 flex items-center text-xl text-white font-bold gap-1 lg:text-3xl'>
+            <span>
 
        <FaBed />
             </span>
