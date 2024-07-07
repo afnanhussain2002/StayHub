01793ff7ed7e8a58a2340b287aff72444114c878
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-07 10:14:35 +0600

    Add responsive topbar

--- client/src/sheard/header/TopBar.jsx
+++ client/src/sheard/header/TopBar.jsx
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom";
 
 const TopBar = () => {
   return (
-    <div className="bg-white w-full font-semibold">
+    <div className="bg-white w-full font-semibold py-1">
 
     <div className=" flex justify-around max-w-screen-xl mx-auto">
-      <div className="flex items-center gap-1 text-back-main-color">
+      <div className="flex items-center gap-1 font-bold text-back-main-color text-xs lg:text-sm">
         <FaLocationDot />
         <p>96 Ernser Vista Suite 437, NY, US</p>
       </div>
-      <div className="flex items-center gap-1 text-back-main-color">
+      <div className="flex items-center gap-1 font-bold text-back-main-color text-xs lg:text-sm">
       <IoIosCall />
         <p> (123) 456-78-910 </p>
       </div>
-      <div className="flex items-center gap-1 text-back-main-color">
+      <div className="flex items-center gap-1 font-bold text-back-main-color text-xs lg:text-sm">
         <Link to={'/login'}>Login |</Link>
         <Link to={'/register'}>Register</Link>
       </div>
