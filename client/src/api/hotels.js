73225aa97bb209839f3d6d68da3a08f316e391b5
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-09 22:30:53 +0600

    Add a function for get all hotels on client side

--- client/src/api/hotels.js
+++ client/src/api/hotels.js
@@ -1,7 +1,16 @@
 import axiosSecure from "./axiosSecure"
 
+// post hotel
+
 export const addHotel = async(hotel) =>{
     const {data} = await axiosSecure.post('/hotels', hotel)
 
+    return data
+}
+
+// get all hotels
+
+export const getAllHotels = async () =>{
+    const {data} = await axiosSecure('/hotels')
     return data
 }
