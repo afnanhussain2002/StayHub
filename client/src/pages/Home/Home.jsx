1e77477d014ef5c275950359995d026af180390d
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-10 07:11:12 +0600

    Remove the fake data form client side

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,9 @@
 import AllCategories from "../../components/AllCategories/AllCategories";
-import AllProducts from "../../components/allProducts/AllProducts";
 import GetAllHotels from "../../components/getAllHotels/GetAllHotels";
 import ImageBG from "../../components/reUseComponents/imageBG";
 import AboutSection from "./HomeSections/AboutSection";
 import HeroSection from "./HomeSections/HeroSection";
-import MainFeatures from "./HomeSections/MainFeatures";
+
 
 
 const Home = () => {
@@ -16,7 +15,6 @@ const Home = () => {
             <ImageBG image={"[url('https://i.ibb.co/74Vr28t/3d-rendering-beautiful-luxury-bedroom-suite-hotel-with-tv-105762-2301.jpg')]"}>
             <AllCategories/>
             </ImageBG>
-            <AllProducts/>
             <GetAllHotels/>
           
         </>
