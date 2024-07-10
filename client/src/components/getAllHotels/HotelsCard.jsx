bf9b0850420be5852c4b1f67d7a0689c75294a6c
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-10 07:56:50 +0600

    Show images on hotels details page

--- client/src/components/getAllHotels/HotelsCard.jsx
+++ client/src/components/getAllHotels/HotelsCard.jsx
@@ -3,41 +3,42 @@ import { Link } from "react-router-dom";
 const HotelsCard = ({ hotel }) => {
   return (
     <div className="p-4 w-1/3">
-    <div className="h-full border-2 border-back-main-color rounded-lg overflow-hidden">
-      <img
-        className="lg:h-64 md:h-36 w-full object-cover object-center"
-        src={hotel.bannerImage.data.display_url}
-        alt={hotel.hotelName}
-      />
-      <div className="p-6">
-        <h2 className="tracking-widest text-xs title-font font-medium text-black mb-1">
-          {hotel.category}
-        </h2>
-      
-        <p className="leading-relaxed mb-3 text-back-main-color font-semibold text-2xl">{hotel.hotelName}</p>
-        <div className="flex items-center flex-wrap ">
-          <Link to={`/hotel/${hotel._id}`}>
-          <button className="bg-back-main-color p-2 text-white font-bold rounded-md">
-            Details
-          </button>
-          
-          </Link>
-          <span className="text-black mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-black">
-            {hotel.host.name}
-          </span>
-          <span className=" inline-flex items-center leading-none">
-            <img
-              width={30}
-              height={30}
-              className="rounded-lg"
-              src={hotel.host.image}
-              alt=""
-            />
-          </span>
+      <div className="h-full border-2 border-back-main-color rounded-lg overflow-hidden">
+        <img
+          className="lg:h-64 md:h-36 w-full object-cover object-center"
+          src={hotel.bannerImage.data.display_url}
+          alt={hotel.hotelName}
+        />
+        <div className="p-6">
+          <h2 className="tracking-widest text-xs title-font font-medium text-black mb-1">
+            {hotel.category}
+          </h2>
+
+          <p className="leading-relaxed mb-3 text-back-main-color font-semibold text-2xl">
+            {hotel.hotelName}
+          </p>
+          <div className="flex items-center flex-wrap ">
+            <Link to={`/hotel/${hotel._id}`}>
+              <button className="bg-back-main-color p-2 text-white font-bold rounded-md">
+                Details
+              </button>
+            </Link>
+            <span className="text-black mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-black">
+              {hotel.host.name}
+            </span>
+            <span className=" inline-flex items-center leading-none">
+              <img
+                width={30}
+                height={30}
+                className="rounded-lg"
+                src={hotel.host.image}
+                alt=""
+              />
+            </span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
--- client/src/pages/hotelDetails/HotelDetails.jsx
+++ client/src/pages/hotelDetails/HotelDetails.jsx
@@ -1,13 +1,27 @@
 import { useLoaderData } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import Loader from "../../components/reUseComponents/Loader";
 
 
 const HotelDetails = () => {
+    const {loading} = useAuth()
     const hotel = useLoaderData()
     console.log(hotel);
+    if (loading) return <Loader/>
     return (
-        <div>
+        <>
+
+        <section className="grid grid-cols-2 gap-2 mt-8 max-w-7xl mx-auto justify-center lg:grid-cols-4">
+            <img src={hotel.bannerImage.data.display_url} alt="" className="w-full h-96 object-cover col-span-2 border-4 border-back-main-color rounded-lg"/>
+            <img src={hotel.roomImg1} alt="" className="w-full h-96 object-cover border-4 border-back-main-color rounded-lg"/>
+            <img src={hotel.roomImg2} alt="" className="w-full h-96 object-cover border-4 border-back-main-color rounded-lg"/>
+            <img src={hotel.roomImg3} alt="" className="w-full h-96 object-cover border-4 border-back-main-color rounded-lg"/>
+            <img src={hotel.roomImg4} alt="" className="w-full h-96 object-cover border-4 border-back-main-color rounded-lg"/>
+
+        </section>
+
             
-        </div>
+        </>
     );
 };
 
