30f9e5bc84871f3b0bb2971b0fdbc322ee11c6eb
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-11 08:38:47 +0600

    Add some design on menus

--- client/src/layouts/Dashboard/DashBoard.jsx
+++ client/src/layouts/Dashboard/DashBoard.jsx
@@ -3,12 +3,12 @@ import MenuDashboard from "./menuDashboard/menuDashboard";
 
 const DashBoard = () => {
     return (
-        <div className="flex">
+        <div className="min-h-screen md:flex">
              {/* menu */}
              <div className="max-w-lg">
                 <MenuDashboard/>
              </div>
-             <div className="max-w-5xl">
+             <div className="bg-green-600 ml-64 flex-1">
               content
              </div>
         </div>
--- client/src/layouts/Dashboard/menuDashboard/menuDashboard.jsx
+++ client/src/layouts/Dashboard/menuDashboard/menuDashboard.jsx
@@ -2,16 +2,17 @@ import { Link } from "react-router-dom";
 
 
 const MenuDashboard = () => {
+    
     return (
-        <div className="flex flex-col">
-            <div className="flex flex-col gap-4">
-                <Link to={'/dashboard'}>Home</Link>
+        <div className="flex flex-col bg-back-main-color h-full p-12 text-white font-bold text-2xl">
+            <div className="flex flex-col gap-4 flex-1">
+                <Link to={'/dashboard'}>Dashboard</Link>
                 <Link to={'/dashboard'}>Home</Link>
                 <Link to={'/dashboard'}>Home</Link>
                 <Link to={'/dashboard'}>Home</Link>
             </div>
             <div>
-                <Link>Logout</Link>
+                <button className="bg-white p-2 rounded-md text-back-main-color">Logout</button>
             </div>
         </div>
     );
