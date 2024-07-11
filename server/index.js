a9f2d04d19be2899b17e5f3257e24cdb12fd13ab
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-11 18:32:49 +0600

    Write an api for get all users

--- server/index.js
+++ server/index.js
@@ -111,6 +111,12 @@ async function run() {
      res.send(result)
     })
 
+    // get all users 
+    app.get('/users', async(req,res) =>{
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
