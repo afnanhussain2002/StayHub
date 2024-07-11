2c624bfc42e91e0a9eae03f1fd4e0fb5dc7de56f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-11 10:07:44 +0600

    Now hotel details is also private routes

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
             
             {
                 path:'/hotel/:id',
-                element:<HotelDetails/>,
+                element:<PrivateRoutes><HotelDetails/></PrivateRoutes>,
                 loader: ({params}) => getSingleHotels(params.id)
             
             }
