6650cb62ce5a90401afbfdced068ad560e5c5c10
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-12 21:48:21 +0600

    Make a hook for get user role

--- client/src/hooks/useRole.jsx
+++ client/src/hooks/useRole.jsx
@@ -1,10 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import { getUserRole } from "../api/users";
 
 const useRole = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const {user, loading} = useAuth()
+    const {data:role, isLoading} = useQuery({
+        enabled: !loading && !!user?.email,
+        queryFn:async() => await getUserRole(user?.email),
+        queryKey:['role']
+    })
+    return [role, isLoading]
 };
 
 export default useRole;
