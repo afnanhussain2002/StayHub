9755dc2596a9838e6ffd93f65ede32d50fda51e9
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-12 09:36:53 +0600

    Show paymnent after calculate on payment page

--- client/src/pages/Payment/CheckoutForm.jsx
+++ client/src/pages/Payment/CheckoutForm.jsx
@@ -3,95 +3,99 @@ import { useEffect, useState } from "react";
 import { sendBookingPrice } from "../../api/payment";
 import useAuth from "../../hooks/useAuth";
 
-
-const CheckoutForm = ({price}) => {
-  const {user} = useAuth()
-    const stripe = useStripe();
+const CheckoutForm = ({ price }) => {
+  const { user } = useAuth();
+  const stripe = useStripe();
   const elements = useElements();
-  const [err, setErr] = useState('')
-  const [clientSecret, setClientSecret] = useState()
-  const bookingAmount = price - (price * 0.8 )
- 
+  const [err, setErr] = useState("");
+  const [clientSecret, setClientSecret] = useState();
+  const bookingAmount = price - price * 0.8;
+
   // post payment
-  useEffect(() =>{
-    const fetchData = async () =>{
+  useEffect(() => {
+    const fetchData = async () => {
       try {
-        const data = await sendBookingPrice(bookingAmount)
+        const data = await sendBookingPrice(bookingAmount);
         console.log(data.clientSecret);
-        setClientSecret(data.clientSecret)
+        setClientSecret(data.clientSecret);
       } catch (error) {
         console.log(error);
       }
+    };
+    fetchData();
+  }, [bookingAmount]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
     }
-    fetchData()
-  },[bookingAmount])
-    const handleSubmit = async e =>{
-        e.preventDefault()
+    const card = elements.getElement(CardElement);
 
-        if (!stripe || !elements) {
-            return
-        }
-        const card = elements.getElement(CardElement);
-
-        if (card == null) {
-          return;
-        }
-
-        const {error, paymentMethod} = await stripe.createPaymentMethod({
-          type:'card',
-          card
-        })
-
-        if (error) {
-          console.log(error);
-          setErr(error.message)
-        }else{
-          console.log(paymentMethod);
-          setErr('')
-        }
-
-        // confirm payment
-
-        const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(clientSecret,{
-          payment_method:{
-            card:card,
-            billing_details:{
-               email:user?.email || 'anonymous',
-               name: user?.displayName || 'anonymous'
-            }
-          }
-        })
-        if (confirmError) {
-          console.log('confirm error',confirmError);
-        }else{
-            console.log('payment Intent', paymentIntent);
-        }
-
+    if (card == null) {
+      return;
     }
-    return (
-        <form onSubmit={handleSubmit}>
-              <CardElement
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log(error);
+      setErr(error.message);
+    } else {
+      console.log(paymentMethod);
+      setErr("");
+    }
+
+    // confirm payment
+
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.displayName || "anonymous",
+          },
+        },
+      });
+    if (confirmError) {
+      console.log("confirm error", confirmError);
+    } else {
+      console.log("payment Intent", paymentIntent);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
         options={{
           style: {
             base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
               },
             },
             invalid: {
-              color: '#9e2146',
+              color: "#9e2146",
             },
           },
         }}
       />
-      <button type="submit" className="p-1 bg-back-main-color rounded text-white font-bold disabled:bg-slate-400" disabled={!stripe || !clientSecret}>
+      <p>Pay 20% of the full payment. So you have to pay ${bookingAmount} for confirm the booking</p>
+      <button
+        type="submit"
+        className="p-1 bg-back-main-color rounded text-white font-bold disabled:bg-slate-400"
+        disabled={!stripe || !clientSecret}
+      >
         Pay
       </button>
       <p className="text-red-600">{err}</p>
-        </form>
-    );
+    </form>
+  );
 };
 
 export default CheckoutForm;
