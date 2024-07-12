1fe0508754a724f8a7d44f26d137357060ddadc2
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-12 11:34:59 +0600

    make a route for payment history

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -14,6 +14,7 @@ import HotelsByHost from "../layouts/Dashboard/HostDashboard/HotelsByHost";
 import AllUsers from "../layouts/Dashboard/AdminDashboard/AllUsers";
 import { getAllUsers } from "../api/users";
 import Payment from "../pages/Payment/Payment";
+import PaymentHistory from "../pages/Payment/PaymentHistory";
 
 const router = createBrowserRouter([
   {
@@ -74,6 +75,10 @@ const router = createBrowserRouter([
       {
         path:'payment',
         element:<Payment/>
+      },
+      {
+        path:'paymentHistory',
+        element:<PaymentHistory/>
       }
 
     ],
