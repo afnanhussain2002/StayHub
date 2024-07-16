b2b328049d5f8f76899089bea048d33ded369461
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-16 11:05:38 +0600

    Add navlink with active style on dashboard menu

--- client/src/layouts/Dashboard/menuDashboard/menuDashboard.jsx
+++ client/src/layouts/Dashboard/menuDashboard/menuDashboard.jsx
@@ -1,27 +1,79 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Logo from "../../../sheard/header/Logo";
 import useRole from "../../../hooks/useRole";
 
-
 const MenuDashboard = () => {
-    const [role] = useRole()
+  const [role] = useRole();
 
-    return (
-        <div className="flex flex-col bg-back-main-color h-full p-12 text-white font-bold text-xl">
-            <div className="flex flex-col gap-4 flex-1">
-                <Logo/>
-                <Link to={'/dashboard'}>Dashboard</Link>
-                <Link to={'addHotel'}>Add Hotel</Link>
-                <Link to={'/dashboard'}>View Orders</Link>
-                <Link to={'yourHotels'}>Your Hotels</Link>
-                {role ==='admin' && <Link to={'allUsers'}>All Users</Link>}
-                <Link to={'paymentHistory'}>Payment History</Link>
-            </div>
-            <div>
-                <button className="bg-white p-2 rounded-md text-back-main-color">Logout</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col bg-back-main-color h-full p-12 text-white font-bold text-xl">
+      <div className="flex flex-col gap-4 flex-1">
+        <Logo />
+        <NavLink
+          to={"/dashboard"}
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "" : ""
+          }
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to={"addHotel"}
+          className={({ isActive, isPending }) =>
+            isPending
+              ? ""
+              : isActive
+              ? "bg-white text-back-main-color rounded pl-1"
+              : ""
+          }
+        >
+          Add Hotel
+        </NavLink>
+        <NavLink
+          to={"viewOrders"}
+          className={({ isActive, isPending }) =>
+            isPending
+              ? ""
+              : isActive
+              ? "bg-white text-back-main-color rounded pl-1"
+              : ""
+          }
+        >
+          View Orders
+        </NavLink>
+        <NavLink
+          to={"yourHotels"}
+          className={({ isActive, isPending }) =>
+            isPending
+              ? ""
+              : isActive
+              ? "bg-white text-back-main-color rounded pl-1"
+              : ""
+          }
+        >
+          Your Hotels
+        </NavLink>
+        {role === "admin" && <Link to={"allUsers"}>All Users</Link>}
+        <NavLink
+          to={"paymentHistory"}
+          className={({ isActive, isPending }) =>
+            isPending
+              ? ""
+              : isActive
+              ? "bg-white text-back-main-color rounded pl-1"
+              : ""
+          }
+        >
+          Payment History
+        </NavLink>
+      </div>
+      <div>
+        <button className="bg-white p-2 rounded-md text-back-main-color">
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MenuDashboard;
